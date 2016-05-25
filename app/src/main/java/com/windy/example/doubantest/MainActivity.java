fbd3abaf45361cbe2e4c43127f805d7fae3b5afc
windog <windiscall@gmail.com>
2016-05-25 15:09:10 +0800

    viewpager tabs and viewpagers

--- app/src/main/java/com/windy/example/doubantest/MainActivity.java
+++ app/src/main/java/com/windy/example/doubantest/MainActivity.java
@@ -1,13 +1,91 @@
 package com.windy.example.doubantest;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Window;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+public class MainActivity extends FragmentActivity {
+
+    private PagerSlidingTabStrip pagerTab;
+    private ViewPager pager;
+    public final int NUM_PAGES = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        pagerTab = (PagerSlidingTabStrip) findViewById(R.id.pager_tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+        pagerTab.setViewPager(pager);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        float padding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, dm);
+
+    }
+
+    private class PagerAdapter extends FragmentPagerAdapter{
+
+        private final String[] TITLES = getResources().getStringArray(R.array.pager_name);
+
+        private PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * This method may be called by the ViewPager to obtain a title string
+         * to describe the specified page. This method may return null
+         * indicating no title for this page. The default implementation returns
+         * null.
+         *
+         * @param position The position of the title requested
+         * @return A title for the requested page
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        /**
+         * Return the Fragment associated with a specified position.
+         *
+         * @param position
+         */
+        @Override
+        public Fragment getItem(int position) {
+            Bundle bundle = new Bundle();
+            Fragment pagerFragment;
+
+            if (position == 0) {
+                pagerFragment = new PagerFragment();
+            } else {
+                pagerFragment = new PagerFragment();
+            }
+
+            bundle.putInt("page_num",position);
+            pagerFragment.setArguments(bundle);
+
+            return pagerFragment;
+        }
+
+        /**
+         * Return the number of views available.
+         */
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
---
+++ app/src/main/java/com/windy/example/doubantest/PagerFragment.java
@@ -0,0 +1,33 @@
+package com.windy.example.doubantest;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by windog on 2016/5/25.
+ */
+public class PagerFragment extends Fragment {
+    private int pageNum;
+    private TextView tv;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View pager = inflater.inflate(R.layout.pager_fragment,container,false);
+
+        tv = (TextView) pager.findViewById(R.id.tv);
+        Bundle bundle = getArguments();
+        pageNum = bundle.getInt("page_num");
+        tv.setText(""+pageNum);
+
+        return pager;
+    }
+
+
+}
